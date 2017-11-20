9ce0e75f38196d248043b914ca696e9395a1404f
Fatima-Zahrae <fatimaelkessab17@gmail.com>
2017-11-20 10:08:17 +0100

    el automigrate creado en el fichero migration.js y tablas creadas

---
+++ server/boot/migration.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+'use strict';
+
+var config = require('../config.local.js')
+
+module.exports = function(app) {
+    //data sources
+    var mysqlDs = app.dataSources.mysqlDs;
+    var Usuario = app.models.Usuario;
+    //var Role = app.models.Role;
+    //var RoleMapping = app.models.RoleMapping;
+    
+    if(process.env.AUTOMIGRATE) {
+        mysqlDs.automigrate(null, function (err) {
+            if (err) console.log(err);
+            console.log('> Models migrated to tables');
+            /*Usuario.create(
+                    {
+                        nombre: 'Administrador',
+                        apellidos: 'Propietario de la aplicacion',
+                        email: config.admin_email,
+                        password: config.admin_password
+                    }
+            , function (err, user) {
+                if (err)
+                    throw err;
+            });*/
+
+        });
+    }
+}
+
+
