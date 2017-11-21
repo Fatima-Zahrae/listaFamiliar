af65161de0b15429826f47bac89071627605e657
Fatima-Zahrae <fatimaelkessab17@gmail.com>
2017-11-21 11:20:50 +0100

    el usuario que hace el post de lista familiar es el owner

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Listafamiliar) {
-
+    //hacemos k el usuario q haga el post de lista familiar sera el owner
+        Listafamiliar.beforeRemote('create', function (context, listaFamiliar, next) {
+          context.args.data.owner = context.req.accessToken.userId;
+        next();
+    });
+    
+    /*Listafamiliar.afterRemote('find', function (context, listaFamiliar, next) {
+          var 
+        next();
+    });*/
+    
 };
