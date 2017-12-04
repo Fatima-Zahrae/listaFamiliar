9b2aa7546c76a27503b55b3d5ffcc8da2f68b67f
Fatima-Zahrae <fatimaelkessab17@gmail.com>
2017-12-04 07:48:21 +0100

    rechazar lista eliminida

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -73,17 +73,5 @@ module.exports = function (Usuario) {
     };
 
 
-    /**
-     * este es ek¡l metodo para rechazar la solicitud del usuario solicitante
-     * @param {object} context contexto
-     * @param {Function(Error, object)} callback
-     */
-
-    Usuario.prototype.rechazarSolicitud = function (context, callback) {
-        var arraydeUsuarios;
-        var IdAutenticado = context.req.access.Token;
-
-        callback(null, arraydeUsuarios);
-    };
 
 };
