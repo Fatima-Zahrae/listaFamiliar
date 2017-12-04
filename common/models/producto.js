ed55b69e143d553a7907d52c24cb05ecd16bf2c4
Fatima-Zahrae <fatimaelkessab17@gmail.com>
2017-12-04 12:27:18 +0100

    sin terminar

--- common/models/producto.js
+++ common/models/producto.js
@@ -1,5 +1,46 @@
 'use strict';
 
 module.exports = function(Producto) {
+    /**
+ * metodo de limpiar lista
+ * @param {object} contexto contexto
+ * @param {Function(Error, array)} callback
+ */
+
+Producto.limpiarLista = function(contexto, callback) {
+  
+  //temgo el id del usuario autenticado
+  var IdAutenticado = contexto.req.accessToken.userId;
+  
+  //accedo al modelo de productos a traves de productos
+  var Usuario = Producto.app.models.Usuario;
+ 
+  //hago in find de Ususarios para poder tener una instancia de usuarioCompleto
+    Usuario.findById(IdAutenticado ,function(err , usuarioCompleto){
+        if(err)callback(err);
+        
+        //tengo el IdListaFamiliar del autenticado
+        var IdlistaAuten = usuarioCompleto.listaFamiliarId;
+        
+            Producto.updateAll({listaFamiliarId:IdlistaAuten}, {comprar:false},function(err , numeroProdActuali){
+                if(err)callback(err);
+  
+               
+                Producto.find({
+                            where: {
+                                listaFamiliarId: IdlistaAuten
+                            }
+                        },
+                                function (err, arrayProductos) {
+                                    console.log(arrayProductos);
+                                    if (err)callback(err);
+                                    callback(null, arrayProductos);
+
+                                }
+                        );
+            });
+    });
+};
+
 
 };
